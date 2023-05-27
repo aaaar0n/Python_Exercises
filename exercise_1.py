35415c51c95aa6e59931d33885ce919d1761fdba
Aaron <ardelacruz1624@gmail.com>
2023-05-28 01:06:35 +0800

    Added exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,1 @@
+print('Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, Like a diamond in the sky.\n\tTwinkle, twinkle, little star, How I wonder what you are!')
